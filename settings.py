9328599d17bcfba38f0aac15fec91f2e189195f4
ahan <lqlu0017@gmail.com>
2017-06-17 20:42:06 +0800

    外星人

--- settings.py
+++ settings.py
@@ -5,16 +5,23 @@ class Settings():
 	def __init__(self):
 		"""初始化游戏的设置"""
 		# 屏幕设置
-		self.screen_width = 1200
-		self.screen_height = 600
+		self.screen_width = 640
+		self.screen_height = 640
 		self.bg_color = (230, 230, 230)
 
 		# 飞船的设置
-		self.ship_speed_factor = 1.5
+		self.ship_speed_factor = 2.5
+		self.ship_limit = 1
 
 		# 子弹设置
-		self.bullet_speed_factor = 1
+		self.bullet_speed_factor = 5
 		self.bullet_width = 3
 		self.bullet_height = 5
 		self.bullet_color = 60, 60, 60
-		self.bullets_allowed = 3
+		self.bullets_allowed = 30
+
+		# 外星人设置
+		self.alien_speed_factor = 1
+		self.fleet_drop_speed = 10
+		# fleet_direction为1表示向右移，为-1表示向左移
+		self.fleet_direction = 1
--- ship.py
+++ ship.py
@@ -22,6 +22,8 @@ class Ship():
 		# 移动标志
 		self.moving_left = False
 		self.moving_right = False
+		self.moving_up = False
+		self.moving_down = False
 
 	def update(self):
 		"""根据移动标志调整飞船的位置"""
@@ -29,6 +31,10 @@ class Ship():
 			self.center -= self.ai_settings.ship_speed_factor
 		if self.moving_right and self.rect.right < self.screen_rect.right:
 			self.center += self.ai_settings.ship_speed_factor
+		# if self.moving_up and self.rect.up > 0:
+		# 	self.center -= self.ai_settings.ship_speed_factor
+		# if self.moving_down and self.rect.right < self.screen_rect.right:
+		# 	self.center += self.ai_settings.ship_speed_factor
 
 		# 根据self.center更新rect对象
 		self.rect.centerx = self.center
@@ -36,3 +42,7 @@ class Ship():
 	def blitme(self):
 		"""在指定位置绘制飞船"""
 		self.screen.blit(self.image, self.rect)
+
+	def center_ship(self):
+		"""让飞船在屏幕上居中"""
+		self.center = self.screen_rect.centerx
